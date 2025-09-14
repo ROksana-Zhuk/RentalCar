5f265cd8d2ba2af21b05fa3a32247d5f7839be03
ROksana-Zhuk <roksi.sun.forever@gmail.com>
2025-09-14 21:52:35 +0300

    Fix wrong text displayed at loading on CarList page

--- src/components/CarsList/CarsList.jsx
+++ src/components/CarsList/CarsList.jsx
@@ -1,6 +1,7 @@
 import { useSelector } from "react-redux";
 import CarCard from "../CarCard/CarCard.jsx";
 import { selectCars } from "../../redux/cars/selectors.js";
+import { selectIsLoading } from '../../redux/ui/slice.js';
 import css from './CarsList.module.css';
 
 
@@ -8,8 +9,12 @@ export default function CarList() {
 
 
     const allCars = useSelector(selectCars);
+    const isLoading = useSelector(selectIsLoading);
 
     if (!allCars || allCars.length === 0) {
+        if (isLoading) {
+            return null;
+        }
         return (
             <div className={css.emptyContainer || ''}>
                 <p className={css.emptyText || ''}>No cars found matching your search.</p>
